4678095245289aee71aaafb267978c723fc5bb76
Guilherme David <guilhermedavidrk@gmail.com>
2023-01-29 18:30:38 -0300

    :lock: feat: add route availability and render hr

---
+++ src/pages/api/users/[username]/availability.api.ts
@@ -0,0 +1,93 @@
+import { prisma } from '@/lib/prisma'
+import dayjs from 'dayjs'
+import { NextApiRequest, NextApiResponse } from 'next'
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  if (req.method !== 'GET') {
+    return res.status(405).end()
+  }
+
+  // http://localhost:3000/api/users/manogui/availability?date=2023-01-30
+
+  const username = String(req.query.username)
+
+  const { date } = req.query
+
+  if (!date) {
+    return res.status(400).json({ message: 'Date not provided' })
+  }
+
+  const user = await prisma.user.findUnique({
+    where: {
+      username,
+    },
+  })
+
+  if (!user) {
+    return res.status(400).json({ message: 'User does not exist' })
+  }
+
+  const referenceDate = dayjs(String(date))
+
+  const isPastDate = referenceDate.endOf('day').isBefore(new Date())
+
+  if (isPastDate) {
+    return res.json({
+      message: 'Date is past',
+      possibleTimes: [],
+      availableTimes: [],
+    })
+  }
+
+  const userAvailability = await prisma.userTimeInterval.findFirst({
+    where: {
+      user_id: user.id,
+      week_day: referenceDate.get('day'),
+    },
+  })
+
+  if (!userAvailability) {
+    return res.json({
+      message: 'No availability',
+      possibleTimes: [],
+      availableTimes: [],
+    })
+  }
+
+  const { time_end_in_minutes, time_start_in_minutes } = userAvailability
+
+  // Agendamentos so poderão ser feitos de hora em hora
+  const startHour = time_start_in_minutes / 60
+  const endHour = time_end_in_minutes / 60
+
+  const possibleTimes = Array.from({
+    length: endHour - startHour,
+  }).map((_, i) => {
+    return startHour + i
+  })
+
+  const blockedTimes = await prisma.scheduling.findMany({
+    select: {
+      date: true,
+    },
+    where: {
+      user_id: user.id,
+      date: {
+        // greater than or equal
+        gte: referenceDate.set('hour', startHour).toDate(),
+        lte: referenceDate.set('hour', endHour).toDate(),
+      },
+    },
+  })
+
+  const availableTimes = possibleTimes.filter((time) => {
+    return !blockedTimes.some(
+      (blockedTime) => blockedTime.date.getHours() === time,
+    )
+  })
+
+  return res.json({ possibleTimes, availableTimes })
+}
