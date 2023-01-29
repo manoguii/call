667b2b33e7c9de7e75c3844ca35a010adc814144
Guilherme David <guilhermedavidrk@gmail.com>
2023-01-29 11:37:00 -0300

    :construction: html: create component calendar

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -43,11 +43,35 @@ export function Calendar() {
             <td></td>
             <td></td>
             <td></td>
+            <td></td>
             <td>
               <CalendarDay>1</CalendarDay>
             </td>
             <td>
-              <CalendarDay>2</CalendarDay>
+              <CalendarDay disabled>2</CalendarDay>
+            </td>
+            <td>
+              <CalendarDay>3</CalendarDay>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <CalendarDay>1</CalendarDay>
+            </td>
+            <td>
+              <CalendarDay>1</CalendarDay>
+            </td>
+            <td>
+              <CalendarDay>1</CalendarDay>
+            </td>
+            <td>
+              <CalendarDay>1</CalendarDay>
+            </td>
+            <td>
+              <CalendarDay disabled>2</CalendarDay>
+            </td>
+            <td>
+              <CalendarDay>3</CalendarDay>
             </td>
             <td>
               <CalendarDay>3</CalendarDay>
