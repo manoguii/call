0164a3c78de90b65c9ace130c8ed0000bde0c525
Guilherme David <guilhermedavidrk@gmail.com>
2023-01-30 16:49:16 -0300

    :lock: feat: create route schedule

--- src/pages/schedule/[username]/scheduleForm/confirmStep/index.tsx
+++ src/pages/schedule/[username]/scheduleForm/confirmStep/index.tsx
@@ -1,6 +1,8 @@
+import { api } from '@/lib/axios'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { Button, Text, TextArea, TextInput } from '@ignite-ui/react'
 import dayjs from 'dayjs'
+import { useRouter } from 'next/router'
 import { CalendarBlank, Clock } from 'phosphor-react'
 import { useForm } from 'react-hook-form'
 import { z } from 'zod'
@@ -33,8 +35,21 @@ export function ConfirmStep({
     resolver: zodResolver(confirmFormSchema),
   })
 
-  function handleConfirmSchedule(data: ConfirmFormData) {
-    console.log(data)
+  const router = useRouter()
+
+  const username = String(router.query.username)
+
+  async function handleConfirmSchedule(data: ConfirmFormData) {
+    const { email, name, observations } = data
+
+    await api.post(`/users/${username}/schedule`, {
+      name,
+      email,
+      observations,
+      date: schedulingDate,
+    })
+
+    onCancelConfirmation()
   }
 
   const describeDate = dayjs(schedulingDate).format('DD[ de ]MMMM[ de ]YYYY')
